1058271e795ccc325ff8bcdafdcc9866e95b9b98
LisaTheTechnologue <jolisa.chan.store@gmail.com>
2017-12-30 14:30:29 -0500

    tracking page changes

--- PersonalBookLib-TeamProject/Tracking.aspx.cs
+++ PersonalBookLib-TeamProject/Tracking.aspx.cs
@@ -11,14 +11,31 @@ namespace PersonalBookLib_TeamProject
 {
     public partial class Tracking : System.Web.UI.Page
     {
-        
+        private SqlConnection connection = new SqlConnection("Server=localhost;Initial Catalog=Comp229_Project;Integrated Security=True");
         protected void Page_Load(object sender, EventArgs e)
         {
             GetBooks();
         }
         private void GetBooks()
         {
-            
+            SqlCommand comm = new SqlCommand("Select * from Book; ", connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                //display books from database to datalist
+                itemsList.DataSource = reader;
+                itemsList.DataBind();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                dbErrorMessage.Text = ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
